d2a772936666a676ae0902fa4e80c55cf252a35e
William McKenzie <williamqmckenzie@gmail.com>
2022-08-16 18:25:02 -0300

    commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var name
+var name = "Jason"
 
 var health = 100
 var attack = 10
 var defense = 0
 var speed = 40
 
+
+
 function getName(){
-    name = document.getElementById("name").innerText
-    document.getElementById("getName").style.display="none"
-    document.getElementById("menu").classList.remove("hidden")
-    document.getElementById("submit").style.display="none"
+    var x = document.getElementById("name").value
+    document.getElementById("q").innerHTML = x
 }
