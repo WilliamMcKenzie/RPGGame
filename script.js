d7e6bb55f15097c630c69569bc1e5f8bdd834131
William McKenzie <williamqmckenzie@gmail.com>
2022-08-16 19:04:44 -0300

    d

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var name = "Jason"
+var username = sessionStorage.getItem("username")
 
 var health = 100
 var attack = 10
@@ -6,8 +6,22 @@ var defense = 0
 var speed = 40
 
 
-
 function getName(){
-    var x = document.getElementById("name").value
-    document.getElementById("q").innerHTML = x
+    var nameVar = document.getElementById("name")
+    var i = 0
+    if(nameVar.value === ""){
+        nameVar.placeholder = "This field is required";
+        return
+    }
+    else if(nameVar.value.trim().length === 0){
+        window.alert(username)
+        return
+    }
+    username = document.getElementById("name").value;
+    sessionStorage.setItem("username", username)
+    window.location.href="home.html"
+}
+
+function alertName(){
+    window.alert(username)
 }
