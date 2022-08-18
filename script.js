2b722db5137e96d1885de53162912b9e172cc747
William McKenzie <williamqmckenzie@gmail.com>
2022-08-18 19:24:06 -0300

    good struff man

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ var damage = 10
 var defense = 0
 var speed = 40
 
+var enemyLevel
+var enemyHealth
+var enemyDamage
+var enemySpeed
+
 var moves = [{name: "Slam", damage: 12}]
 
 var storage = []
@@ -15,70 +20,91 @@ var equipped = {}
 
 window.onbeforeunload = function() { return "Your work will be lost."; };
 
-
+function moveGenerator(){
+    //get damage
+    var minDamage = 5 + enemyLevel*2, maxDamage = 5 + enemyLevel*4
+    var enemyDamage = 0
+    enemyDamage = Math.floor(Math.random() * (maxDamage - minDamage + 1) + minDamage)
+    //get name
+    var moves = ["Megamind","Laxative Beam","Small Forward","Megabalista","Devastation Lazer","Ravana","Burial","Sacrilige","Stompinater"]
+    var moveName = moves[Math.floor(Math.random() * moves.length)]
+    return {name : moveName, damage : enemyDamage}
+}
 
 //DUNGEON!! 
-//generate enemy and player stats
+//generate enemy and dungeon prerequisites
 function generator(){
 
     document.getElementById("menu").classList.add("hidden")
     document.getElementById("dungeon-div").classList.remove("hidden")
 
     //gen enemy
-    var enemyLevel = Math.floor((Math.random() * level) + 1)
+    enemyLevel = Math.floor((Math.random() * level) + 1)
     var minHealth = 20 + enemyLevel*5, maxHealth = 20 + enemyLevel*10
     var minSpeed = 10 + enemyLevel*3, maxSpeed = 10 + enemyLevel*5
-    //var minDamage = 5 + enemyLevel*2, maxDamage = 5 + enemyLevel*4
 
-    var enemyHealth = 0 
-    //var enemyDamage = 0
     enemySpeed = Math.floor(Math.random() * (maxSpeed - minSpeed + 1) + minSpeed)
     enemyHealth = Math.floor(Math.random() * (maxHealth - minHealth + 1) + minHealth)
-    //enemyDamage = Math.floor(Math.random() * (maxDamage - minDamage + 1) + minDamage)
 
+    //possible names
     var enemies = ["Werewolf","Goblin","Hobgoblin","Mole King","Molanoid","E.T","Samantha","Grotesque Guardian","Kirklander","Silicon Baby","Wet Blanket"]
     var enemyName = enemies[Math.floor(Math.random() * enemies.length)]
 
-    document.getElementById("enemy-stats").textContent = `Health: ${enemyHealth} Level: ${enemyLevel} Speed: ${enemySpeed}`
+    //set enemy content
+    document.getElementById("enemy-stats").textContent = `Health: ${enemyHealth} Speed: ${enemySpeed} Level: ${enemyLevel}`
     document.getElementById("enemy-name").textContent = enemyName
 
-    //gen player
+    //set player content
     document.getElementById("character-name").textContent = username
-    document.getElementById("character-stats").textContent = `Health: ${health} Level: ${level}`
+    document.getElementById("character-stats").textContent = `Health: ${health} Speed: ${speed} Level: ${level}`
 
-    speed > enemySpeed ? playerTurn() : enemyTurn()
+    loadDungeon()
 }
-//load moves
-function playerTurn(){
-    var d = document.getElementById("character-moves")
-    d.classList.remove("hidden")
+//create moves
+function loadDungeon(){
+    var enemyEleName = document.getElementById("enemy-name").textContent
     for(var i = 0; i < moves.length; i++){
+        let x = moves[i].damage
+        let y = moves[i].name
         let button = document.createElement("button");
-        button.innerHTML = moves[i].name;
+        button.innerHTML = y
+        button.classList.add("move")
         button.onclick = function playerAttack(){
-            window.alert(moves[i].damage))
+            var total = (x + damage)+Math.floor(Math.random() * speed/20)
+            window.alert(`You use ${y} on ${enemyEleName} for ${total} damage!`)
+            enemyHealth -= total
+            enemyTurn()
         }
         document.getElementById("character-moves").appendChild(button);
     }
-
+    speed > enemySpeed ? playerTurn() : enemyTurn()
 }
-//enemy uses random attack
+function playerTurn(){
+    document.getElementById("character-stats").textContent = `Health: ${health} Speed: ${speed} Level: ${level}`
+    if (health < 0) death()
+    var d = document.getElementById("character-moves")
+    d.classList.remove("hidden")
+}
 function enemyTurn(){
+    document.getElementById("enemy-stats").textContent = `Health: ${enemyHealth} Speed: ${enemySpeed} Level: ${enemyLevel}`
+    if (enemyHealth < 0) victory()
+    var enemyInfo = moveGenerator()
+    window.alert(`${document.getElementById("enemy-name").textContent} uses ${enemyInfo.name}, dealing ${enemyInfo.damage}!`)
+    health -= enemyInfo.damage
+    playerTurn()
+}
+
+//possible exit functions
+function victory(){
     
 }
-//call on enemy turn
-function moveGenerator(){
-    var moves = ["Megamind","Laxative Beam","Small Forward","Megabalista","Devastation Lazer","Ravana","Burial","Sacrilige","Stompinater"]
-    var moveName = moves[Math.floor(Math.random() * moves.length)]
-    var enemyMoveEle = document.getElementById("enemy-move").innerHTML
-    document.getElementById("enemystats").textContent = `Health: ${health} Damage: ${damage}`
+function death(){
+
 }
-
-
 function retreat(){
     document.getElementById("menu").classList.remove("hidden")
     document.getElementById("dungeon-div").classList.add("hidden")
-    for(var i = 0; i < moves.length-1; i++){
+    for(var i = 0; i < moves.length; i++){
         document.getElementById("character-moves").removeChild(document.getElementById("character-moves").lastElementChild)
     }
 }
